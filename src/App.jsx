8f422eba3c2ad6a326a680ff296bbb6843b02c7b
aoteron <aoteronunez@gmail.com>
2024-05-10 14:46:39 +0200

    Ready to set new context

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ function App() {
 		{ id: 1, title: 'Task 1', isFinished: false },
 		{ id: 2, title: 'Task 2', isFinished: false },
 		{ id: 3, title: 'Task 3', isFinished: false },
+		{ id: 4, title: 'Task 4', isFinished: false },
+		{ id: 5, title: 'Task 5', isFinished: false },
 	];
 
 	console.log(initialTasks);
